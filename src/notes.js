b2c4cb20bf3b03ef6d2b3c510d538a917f92f27e
Abijeet <abijeetpatro@gmail.com>
2015-06-27 18:02:50 +0530

    added comments and some code fixes

--- src/notes.js
+++ src/notes.js
@@ -11,12 +11,18 @@ var NotesApp = require(AppConfig.srcPath + 'notes-app.js');
 var AppError = require(AppConfig.helperPath + 'app-error.js');
 
 var Notes = function() {
+  /**
+   * Fetches the notes present in the given notebook.
+   * @param  {String} notebookID Notebook ID
+   * @param  {function} cbMain   Callback method that is sent an errror object
+   * or the list of notes SORTED by createdOn
+   * @return {undefined}         No return type.
+   */
   var getAllNotesByID = function(notebookID, cbMain) {
-    // Fetch all the notes in a given notebookID
     var notesDb = NotesApp.getNotesDb();
     notesDb.find({
       notebookID: notebookID
-    }, function(err, notes) {
+    }).sort({ createdOn : 1 }).exec(function(err, notes) {
       if (err) {
         return cbMain(new AppError(err, 'There was an error while fetching your notes.'));
       }
@@ -24,6 +30,14 @@ var Notes = function() {
     });
   };
 
+  /**
+   * Updates or Inserts the details of a note in the database
+   * @param  {Object}  noteObj   The note to be updated
+   * @param  {Boolean} isNewNote Determies if it's a new note.
+   * @param  {function}  cbMain  Callback function. Is sent the error object if
+   * there was an error, else is sent the updated / inserted note object.
+   * @return {undefined}         No return type.
+   */
   var modifyNote = function(noteObj, isNewNote, cbMain) {
     if (!validateNote(noteObj, isNewNote)) {
       // TODO Validation error.
@@ -57,6 +71,15 @@ var Notes = function() {
       });
     }
 
+    /**
+     * Callback method called after a note has been inserted or updated.
+     * This adds the original element back to the note object and calls the
+     * original callback method sent as parameter to `modifyNote`
+     * @param  {error} err     Error object returned if there was an error,
+     * else returns null.
+     * @param  {Object} noteObj Updated / Inserted note object
+     * @return {undefined}         No return type.
+     */
     function cbNoteModified(err, noteObj) {
       if (err) {
         cbMain(new AppError(err))
@@ -70,8 +93,16 @@ var Notes = function() {
       noteObj = null, cbMain = null;
       return;
     }
-  };
+  }
 
+  /**
+   * Fetches note details by the ID
+   * @param  {String} noteID ID of the note
+   * @param  {function} cbMain Callback that is sent the note object that
+   * contains the details of the note, if there was an error is returned the
+   * error object.
+   * @return {undefined}        No return type.
+   */
   var getNoteByID = function(noteID, cbMain) {
     var notesDb = NotesApp.getNotesDb();
     if (!noteID) {
@@ -88,7 +119,15 @@ var Notes = function() {
     });
   };
 
+  /**
+   * Private function used to validate a note object. If it's NOT a new note
+   * ensures that the _id parameter has been provided.
+   * @param  {Object}  noteObj   Note object
+   * @param  {Boolean} isNewNote Is this a new note?
+   * @return {Boolean}           true if it's a valid note object, else false.
+   */
   function validateNote(noteObj, isNewNote) {
+    // TODO : More validations are pending.
     if (!noteObj) {
       return false;
     }
@@ -106,6 +145,13 @@ var Notes = function() {
     return true;
   }
 
+  /**
+   * Private function to populate values into the note object before its saved
+   * or updated. Adds the createdOn property if it's a new note.
+   * @param  {Object}  noteObj   Note Object
+   * @param  {Boolean} isNewNote Is this a new note?
+   * @return {Object}            Modified note object
+   */
   function populateValues(noteObj, isNewNote) {
     delete noteObj.noteElem;
     delete noteObj.isBlur;
