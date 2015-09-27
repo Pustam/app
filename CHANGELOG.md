87e85fb0f8180e4a8b1861186ca3ec53ce9372e8
Abijeet <abijeetpatro@gmail.com>
2015-09-27 23:00:19 +0530

    Adding the release file and the changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+
+### v0.3.0 (2015-09-07)
+
+- Re-factored a ton of code, and removed redundant code in the renderer process. Application should be much faster to start now. - #25
+- Fixed the event architecture. - #25
+- Added functionality to add notebooks. - #2
+- Added functionality to move notes between dates. - #29
+- Updated electron version to 0.33.3
+
+---
---
+++ release/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var packager = require('electron-packager');
+
+var options = {
+  "dir" : ".",
+  "name" : "markdown-notes",
+  "platform" : ["linux","win32"],
+  "version" : "0.33.3",
+  "out" : "/home/abijeet/Projects/markdown-notes/releases/",
+  "icon" : "/home/abijeet/Pictures/markdown-notes-ico.png",
+  "app-version" : "0.3.0",
+  "arch" : "x64",
+  "ignore" : ["settings.json", "release", "Gruntfile.js", ".bowerrc",
+    "node_modules/grunt", "node_modules/grunt-jsbeautifier", ".jshintrc"]
+};
+
+console.log('Packaging app with following settings --\n\n');
+console.log(options);
+
+packager(options, function done (err, appPath) {
+  if(err) {
+    console.log('There was an error while packaging the app.');
+    console.log('---------------------------------------\n\n');
+  } else {
+    console.log('App packaged successfully!');    
+  }
+});
