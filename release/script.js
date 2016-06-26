a44f2e74160e4bebc6f6cbbedaa42ad5f301f3b9
Abijeet <abijeetpatro@gmail.com>
2016-06-26 20:59:12 +0530

    Final touches for the 0.4.0 release.

--- release/script.js
+++ release/script.js
@@ -8,7 +8,7 @@ var options = {
   "platform" : ["linux", "win32"],
   "version" : "0.37.6",
   "out" : "/home/abijeet/Projects/markdown-notes/releases/",
-  "icon" : "/home/abijeet/Pictures/markdown-notes-ico.png",
+  "icon" : "/home/abijeet/Pictures/markdown-notes.ico",
   "app-version" : "0.4.0",
   "arch" : "x64",
   "ignore" : ["settings.json", "release", "Gruntfile.js", ".bowerrc",
@@ -26,7 +26,9 @@ packager(options, function done (err, appPath) {
   if(err) {
     console.log('There was an error while packaging the app.');
     console.log('---------------------------------------\n\n');
+    console.log(JSON.stringify(err, null, 2));
   } else {
+    console.log(appPath);
     console.log('App packaged successfully!');
   }
 });
